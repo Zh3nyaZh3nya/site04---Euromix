561a795e527ce69beb882efd593ce778b917ca95
Zh3nyaZh3nya <129783207+Zh3nyaZh3nya@users.noreply.github.com>
2023-07-18 18:23:18 +0600

    добавил ui компонент

---
+++ components/UI/UIButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="custom-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "UIButton"
+}
+</script>
+
+<style scoped lang="scss">
+.custom-btn {
+  border-radius: 100px;
+  background: #FF6B00;
+  padding: 16px 18px;
+  color: #FFF;
+  font-size: 14px;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+</style>
